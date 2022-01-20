b7883ab2283ffa24cde5840d4a4626ddcfde0465
Archatom <tom100034@gmail.com>
2022-01-20 09:15:20 +0800

    V2.3.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,19 +101,19 @@ window.addEventListener('scroll', scrollTop)
 // })
 
 // bots
-window.onload = function () {
-    document.querySelector('.cont_modal').className = "cont_modal";
-}
-var c = 0;
-function open_close() {
-    if (c % 2 == 0) {
-        document.querySelector('.cont_modal').className = "cont_modal cont_modal_active";
-        c++;
-    } else {
-        document.querySelector('.cont_modal').className = "cont_modal";
-        c++;
-    }
-}
+// window.onload = function () {
+//     document.querySelector('.cont_modal').className = "cont_modal";
+// }
+// var c = 0;
+// function open_close() {
+//     if (c % 2 == 0) {
+//         document.querySelector('.cont_modal').className = "cont_modal cont_modal_active";
+//         c++;
+//     } else {
+//         document.querySelector('.cont_modal').className = "cont_modal";
+//         c++;
+//     }
+// }
 
 /*=============== ACCORDION ===============*/
 const accordionItems = document.querySelectorAll('.accordion__item')
